42aa847da6b55a238548ea463e7f86c36e9caa37
fbqweasd <fbqweasd2911@gmail.com>
2018-07-12 09:14:36 +0900

    init +퍄ㅡ

--- init/init.sh
+++ init/init.sh
@@ -3,4 +3,11 @@
 echo "자치차량 init 스크립트"
 
 echo "부저 소리 제거 프로그램 설정중..."
+./../HW_EXE/Buzzer_stop
 cp seting.txt /etc/rc.local
+
+echo "vim 설정 중..."
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim +PluginInstall +aqll
